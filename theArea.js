94610bf2e82ce9350a7e42f8f7c97773a57ad206
QuyenTrung004 <quyentrang22@gmail.com>
2019-02-28 10:53:48 +0700

    su dung toan tu file

---
+++ theArea.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inoutHeight;
+
+inputWidht = prompt("Enter the widht");
+inputHeight = prompt("Enter the height");
+
+let widht = parseInt(inputWidht);
+let height= parseInt(inputHeight);
+
+let area = widht * height;
+document.write("The area is: " + area);
+
